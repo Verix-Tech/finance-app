7c9c043b4ee3a026c9b0325de4bc4e599b35a229
Joao Victor <jvictor200212@gmail.com>
2025-06-25 02:36:59 -0300

    improved responses

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 import logging
 import uuid
+import time
 import utils.utils as utils
 from datetime import datetime
 from config.config import SQLDBConfig, BotConfig, NoSQLDBConfig, UserCache
@@ -55,10 +56,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
     
     user_id = update.effective_user.id
+    user_name = update.effective_user.first_name
     logger.info(f"Received start command from {update.effective_user.username if update.effective_user else 'unknown'}")
     
     if check_user_exists(user_id):
-        await update.message.reply_text("Olá! Você já está registrado. Podemos começar a organizar suas finanças!")
+        response = BotConfig().generate_response("Olá, já estou registrado", user_name)
+        await update.message.reply_text(response.get("message", "Olá! Você já está registrado. Podemos começar a organizar suas finanças!"))
     else:
         await update.message.reply_text("Olá, sou o seu assistente financeiro. Para começar, por favor, compartilhe seu número de telefone comigo.",
                                         reply_markup=ReplyKeyboardMarkup([[KeyboardButton("Compartilhar número de telefone", request_contact=True)]], one_time_keyboard=True, resize_keyboard=True))
@@ -83,7 +86,8 @@ async def handle_contact(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_cache.invalidate(str(user_id))
     user_cache.set(str(user_id), True)
     
-    await update.message.reply_text("Obrigado! Recebi seu número de telefone e te cadastrei no sistema. Agora você pode me enviar mensagens.", reply_markup=ReplyKeyboardRemove())
+    response = BotConfig().generate_response("Acabei de me registrar", user_name)
+    await update.message.reply_text(response.get("message", "Obrigado! Recebi seu número de telefone e te cadastrei no sistema. Agora você pode me enviar mensagens."), reply_markup=ReplyKeyboardRemove())
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message or not update.message.text or not update.effective_user:
@@ -98,15 +102,15 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
     user_name = update.effective_user.first_name
 
-    response = BotConfig().generate_response(user_message)
-    _type = response["api_endpoint"].split("/")[1] if response["api_endpoint"] else None
+    response = BotConfig().generate_response(user_message, user_name)
+    _type = response.get("api_endpoint", "").split("/")[1] if response.get("api_endpoint") else None
 
     if _type == "create-transaction":
         if not response["params"].get("transaction_timestamp"):
             response["params"]["transaction_timestamp"] = datetime.now().strftime("%d/%m/%Y")
         else:
             response["params"]["transaction_timestamp"] = utils.format_date_with_year(response["params"]["transaction_timestamp"])
-    elif _type == "generate-data":
+    elif _type == "generate-report":
         if not response["params"].get("start_date") or not response["params"].get("end_date"): 
             pass
         else:
@@ -114,6 +118,10 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             response["params"]["end_date"] = utils.format_date_with_year(response["params"]["end_date"])
 
     message_id = str(uuid.uuid4())
+
+    response["params"]["payment_category"] = response.get("params", {}).get("payment_category", "0")
+    response["params"]["payment_method_name"] = response.get("params", {}).get("payment_method_name", "0")
+
     # Insert messages into MongoDBdatabase
     NoSQLDBConfig().insert_messages([
         {"message_id": message_id, "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "message": user_message, "is_bot": False, "client_id": str(user_id), "metadata": {"name": user_name}},
@@ -126,7 +134,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         db_response = SQLDBConfig().send_request(
             endpoint=response["api_endpoint"],
             endpoint_var="",
-            method="post",
+            method="post", 
             params=response["params"],
             platform_id=str(user_id)
         )
@@ -134,18 +142,17 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if _type == "create-transaction":
             if db_response.status_code == 201:
                 with open("messages/inserted_transaction.txt", "r", encoding="utf-8") as file:
-                    payment_method_name = response["params"]["payment_method_name"] or "Não informado"
                     await update.message.reply_text(file.read().format(
                         response["params"]["transaction_type"],
                         response["params"]["transaction_revenue"],
                         response["params"]["payment_description"],
-                        response["params"]["payment_category"],
-                        payment_method_name,
+                        BotConfig().CATEGORIAS[response["params"]["payment_category"]],
+                        BotConfig().METODOS_PAGAMENTO[response["params"]["payment_method_name"]],
                         response["params"]["transaction_timestamp"],
                         db_response.json()["data"]["transaction_id"],
                         message_id
                     ))
-        elif _type == "generate-data":
+        elif _type == "generate-report":
             status_code = 0
             while status_code != 200:
                 db_response = SQLDBConfig().send_request(
@@ -158,16 +165,22 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 status_code = db_response.status_code
 
                 if status_code == 200:
-                    csv_buffer = db_response.content.decode("utf-8")
-                    report = utils.format_report(csv_buffer, response["params"]["aggr"]["activated"])
-                    await update.message.reply_photo(utils.create_table_image(report, (4, 4), "Relatório", 600), caption=open("messages/report.txt", "r", encoding="utf-8").read().format(message_id))
+                    try:
+                        csv_buffer = db_response.content.decode("utf-8")
+                        report = utils.format_report(csv_buffer, response.get("params", {}).get("aggr", {}).get("activated", False))
+                        await update.message.reply_photo(utils.create_table_image(report, (4, 4), "Relatório", 600), caption=open("messages/report.txt", "r", encoding="utf-8").read().format(message_id))
+                    except IndexError:
+                        await update.message.reply_text(message_id + "\nVocê não possui transações para gerar relatório no período selecionado.")
+                        return
 
                     return
                 elif status_code == 201:
+                    time.sleep(1)
                     continue
                 else:
-                    await update.message.reply_text(message_id + "\nDesculpe, não consegui processar sua solicitação. Por favor, tente novamente.")
+                    await update.message.reply_text(message_id + "\n" + "\nDesculpe, não consegui processar sua solicitação. Por favor, tente novamente.")
                     return
+                
         elif _type == "update-transaction":
             if db_response.status_code == 201:
                 await update.message.reply_text(message_id + "\nTransação atualizada com sucesso!")
@@ -195,3 +208,25 @@ if __name__ == "__main__":
     main()
     # get_csv = requests.get("http://localhost:8000/task-status/314daa7f-2a3b-4f5d-8a85-5c86e08a51b4", headers={"Authorization": f"Bearer {SQLDBConfig().authenticate()}"})
     # print(get_csv.content.decode("utf-8"))
+    # params = {'start_date': '01/06', 'end_date': '30/06', 'aggr': {'mode': 'day', 'activated': True}}
+    # params["start_date"] = utils.format_date_with_year(params["start_date"])
+    # params["end_date"] = utils.format_date_with_year(params["end_date"])
+    # r = SQLDBConfig().send_request(
+    #     endpoint="/generate-report",
+    #     endpoint_var="",
+    #     method="post",
+    #     params=params,
+    #     platform_id="2140476700"
+    # )
+    # print(r.content.decode("utf-8"))
+    # print(r.request.body)
+
+    # time.sleep(2)
+    # r_1 = SQLDBConfig().send_request(
+    #     endpoint="/task-status",
+    #     endpoint_var=f"{r.json()['data']['task_id']}",
+    #     method="get",
+    #     params={},
+    #     platform_id="2140476700"
+    # )
+    # print(r_1.content.decode("utf-8"))
